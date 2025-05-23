973704943350dd82526000761ec1b16ea1f17108
Your Name <kelvinkiprono659@gmail.com>
2025-05-23 17:26:35 +0300

    Initialize Laravel project and Git repository

--- portfolio/resources/js/app.js
+++ portfolio/resources/js/app.js
@@ -1,7 +1,7 @@
-import './bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+AOS.init({
+    duration: 1000,
+    once: true,
+});
