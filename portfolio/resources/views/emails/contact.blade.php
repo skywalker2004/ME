ec90ffe159107b6da3286010afd8c7edca74a357
Your Name <kelvinkiprono659@gmail.com>
2025-05-23 17:56:53 +0300

    updated contact blade file

---
+++ portfolio/resources/views/emails/contact.blade.php
@@ -0,0 +1,4 @@
+<h1>New Contact Form Submission</h1>
+<p><strong>Name:</strong> {{ $contact->name }}</p>
+<p><strong>Email:</strong> {{ $contact->email }}</p>
+<p><strong>Message:</strong> {{ $contact->message }}</p>
