a06ba064b6582e990ad1cad06c43c89a7baed54f
Your Name <kelvinkiprono659@gmail.com>
2025-05-23 17:52:28 +0300

    skill table migrated

---
+++ portfolio/database/migrations/2025_05_23_105215_create_skills_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('proficiency')->default(0); // Percentage (0-100)
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('skills');
+    }
+};
