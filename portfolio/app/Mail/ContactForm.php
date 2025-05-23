009f782c59ee4217f879d7280b69fd913e53c8c8
Your Name <kelvinkiprono659@gmail.com>
2025-05-23 17:48:48 +0300

    created contact form

---
+++ portfolio/app/Mail/ContactForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactForm extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $contact;
+
+    public function __construct($contact)
+    {
+        $this->contact = $contact;
+    }
+
+    public function build()
+    {
+        return $this->subject('New Contact Form Submission')
+                    ->view('emails.contact')
+                    ->with(['contact' => $this->contact]);
+    }
+}
