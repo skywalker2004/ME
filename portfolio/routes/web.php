4616294493e802ac4ea8665864a2b0c7e24c0937
Your Name <kelvinkiprono659@gmail.com>
2025-05-23 17:57:43 +0300

    updated the routes

--- portfolio/routes/web.php
+++ portfolio/routes/web.php
@@ -1,26 +1,32 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TestimonialController;
+use App\Http\Controllers\BlogPostController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    $projects = \App\Models\Project::all();
+    $services = \App\Models\Service::all();
+    $testimonials = \App\Models\Testimonial::all();
+    return view('welcome', compact('projects', 'services', 'testimonials'));
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::resource('projects', ProjectController::class)->except(['index']);
+    Route::resource('services', ServiceController::class);
+    Route::resource('testimonials', TestimonialController::class);
+    Route::resource('blog_posts', BlogPostController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
